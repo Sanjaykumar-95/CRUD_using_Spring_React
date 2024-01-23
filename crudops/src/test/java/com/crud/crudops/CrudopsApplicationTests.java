da896e912a37ed52d05cd42b8071a5974da4021f
sanjay <sanjaypandu8855@gmail.com>
2024-01-23 21:02:32 +0530

    first commit

---
+++ crudops/src/test/java/com/crud/crudops/CrudopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
